f46155cd1a87161ee04e4276a4124a12714fb5ba
marcos <msl.tcc2020@gmail.com>
2025-06-12 21:53:42 -0300

    Adicionados arquivos do projeto

---
+++ ignition/modules/BookDatabase.ts
@@ -0,0 +1,11 @@
+
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+const BookDatabaseModule = buildModule("BookDatabaseModule", (m) => {
+
+  const bookDatabase = m.contract("BookDatabase");
+
+  return { bookDatabase };
+});
+
+export default BookDatabaseModule;
